8b3ff64980bb082cf7f7599e849f2d340f60da9f
youngG124 <newyoung124@naver.com>
2022-07-20 11:35:43 +0900

    remove images in the page when uploading new image

--- src/main/java/com/example/dataUpload/controller/UploadController.java
+++ src/main/java/com/example/dataUpload/controller/UploadController.java
@@ -3,14 +3,19 @@ package com.example.dataUpload.controller;
 import com.example.dataUpload.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -32,6 +37,7 @@ public class UploadController {
 
     @PostMapping("/uploadAjax")
     public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+        log.info("uploadFile - controller");
 
         List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
@@ -51,7 +57,6 @@ public class UploadController {
             // 실제 파일 이름 (IE나 Edge는 전체 경로가 들어옴)
             String originalName = uploadFile.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
-
             log.info("fileName : " + fileName);
 
             // 날짜 폴더 생성
@@ -88,5 +93,30 @@ public class UploadController {
             uploadPathFolder.mkdirs();
         }
         return folderPath;
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName : " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file : " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
     }
 }
